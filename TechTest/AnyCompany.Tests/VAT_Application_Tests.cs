b419ecfb94e8c088f843a3223b12e1a7c0bac83b
Jernej Goricki <Jernej.Goricki@mishcon.com>
2018-09-26 23:06:16 +0100

    Add VAT calculation tests

--- TechTest/AnyCompany.Tests/VAT_Application_Tests.cs
+++ TechTest/AnyCompany.Tests/VAT_Application_Tests.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Xunit;
 
 namespace AnyCompany.Tests
 {
     public class VAT_Application_Tests
     {
+        [Theory]
+        [InlineData("uk", 0.2)]
+        [InlineData("UK", 0.2)]
+        [InlineData("Uk", 0.2)]
+        [InlineData("uK", 0.2)]
+        [InlineData("", 0)]
+        [InlineData("US", 0)]
+        public void Apply_Correct_Vat(string country, double expectedVat)
+        {
+            //Arrange
+            var sut = new VatApplicator();
+            var order = new Order();
+            //Act
+            sut.ApplyVat(order, new Customer() { Country = country });
+            //Assert
+            Assert.Equal(expectedVat, order.VAT);
+        }
     }
 }
