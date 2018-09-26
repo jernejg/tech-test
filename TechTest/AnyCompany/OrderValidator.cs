jernejg/tech-test

commit 823c06e2e367a6a9c13b413017273f3a04617c07

Fix floating-point comparison
https://www.jetbrains.com/help/resharper/CompareOfFloatsByEqualityOperator.html

TechTest/AnyCompany/OrderValidator.cs
@@ -1,10 +1,12 @@
-﻿namespace AnyCompany
+﻿using System;
+
+namespace AnyCompany
 {
     public class OrderValidator : IOrderValidator
     {
         public bool IsValid(Order o)
         {
-            if (o.Amount == 0)
+            if (Math.Abs(o.Amount) < 0.01)
                 return false;
 
             return true;

commit ad93b72032dcb441aec55af1bad01241d8c147d7

Fix bug introduced in 823c06e2e367a6a9c13b413017273f3a04617c07

TechTest/AnyCompany/OrderValidator.cs
@@ -6,7 +6,7 @@ namespace AnyCompany
     {
         public bool IsValid(Order o)
         {
-            if (Math.Abs(o.Amount) < 0.01)
+            if (o.Amount < 0.01)
                 return false;
 
             return true;