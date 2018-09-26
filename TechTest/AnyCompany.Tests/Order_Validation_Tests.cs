6de1c6a2f45ebbf7024bb0d9665920fede8be1ac
Jernej Goricki <Jernej.Goricki@mishcon.com>
2018-09-26 22:56:15 +0100

    More descriptive test name

--- TechTest/AnyCompany.Tests/Order_Validation_Tests.cs
+++ TechTest/AnyCompany.Tests/Order_Validation_Tests.cs
@@ -10,7 +10,7 @@ namespace AnyCompany.Tests
         [InlineData(0, false)]
         [InlineData(0.01, true)]
         [InlineData(0.02, true)]
-        public void FactMethodName(double val, bool expectedResult)
+        public void Order_Is_Not_Valid_If_Price_Is_Below_Lower_Boundary(double val, bool expectedResult)
         {
             //Arrange
             var sut = new OrderValidator();
