09adebf455282ad333c5ac6e308a8515c0ceecfe
Jernej Goricki <Jernej.Goricki@mishcon.com>
2018-09-27 11:37:05 +0100

    Create a demo project
    The project shows how to wire-up all the components to:
    - Load all customers and their linked orders
    - Place an order, linked to a customer
    
    Further work will have to be done on the PlaceOrder method because it is not intuitive at the moment. Caller needs to know its inner details to use it successfully and lacks of information about what went wrong if the method returns false.
    
    No existing method signatures or access modifiers were changed during this refactoring session.
    
    For automatic resolution of components an IoC container can be use (idea for future refactoring).

---
+++ TechTest/AnyCompany.Console/Program.cs
@@ -0,0 +1,30 @@
+﻿using AnyCompany.PlacingOrders;
+
+namespace AnyCompany.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionStringBuilderProvider = new ConfigFileConnectionStringBuilderProvider("MyConnectionString");
+            var customerRepository = new CustomerRepositoryImplementation(connectionStringBuilderProvider);
+            var orderValidator = new OrderValidator();
+            var vatApplicator = new VatApplicator();
+
+            var orderServiceFactory = new OrderServiceFactory(customerRepository,
+                connectionStringBuilderProvider, orderValidator, vatApplicator);
+
+            var orderService = orderServiceFactory.Create();
+
+            //Load all customers and their linked orders
+            var customers = customerRepository.Load(3);
+
+            //Place an order, linked to a customer
+            var success = orderService.PlaceOrder(new Order()
+            {
+                OrderId = 1,
+                Amount = 1,
+            }, 1);
+        }
+    }
+}
